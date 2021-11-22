b821bcd8c34c052ea84ed5cdb087d3713d29491b
ElizavetaKisel <lizavetakisel00@gmail.com>
2021-11-23 00:15:50 +0300

    test MostRecent

---
+++ src/test/java/by/overone/homework27/AnalysisTest.java
@@ -0,0 +1,25 @@
+package by.overone.homework27;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnalysisTest {
+
+    @ParameterizedTest
+    @MethodSource("dataMostRecent")
+    public void checkMostRecent(String pass, String expected){
+        assertEquals(expected,Analysis.findMostRecent(pass));
+    }
+
+    private static Stream<Arguments> dataMostRecent(){
+        return Stream.of(Arguments.of("the are nine are","are"),
+                Arguments.of("nine root nine root", "nine"),
+                Arguments.of("a a a s o", "a"));
+    }
+
+}
