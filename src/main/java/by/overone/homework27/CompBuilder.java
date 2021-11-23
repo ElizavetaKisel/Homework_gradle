7346c1ef9badc4829541edbe8791d86bf7c95eb7
ElizavetaKisel <lizavetakisel00@gmail.com>
2021-11-23 23:09:38 +0300

    all into eng

--- src/main/java/by/overone/homework27/CompBuilder.java
+++ src/main/java/by/overone/homework27/CompBuilder.java
@@ -4,11 +4,11 @@ public class CompBuilder {
 
     public static StringBuilder shortAppend(StringBuilder result, String text, Hero hero1, Hero hero2){
         result.append("*Romeo & Juliet*"+ "\n");
-        result.append("Самое редко встречающееся слово: " + Analysis.findLeastRecent(text)+ "\n");
-        result.append("Самое часто встречающееся слово: " + Analysis.findMostRecent(text)+ "\n");
-        result.append("Самое длинное слово: " + Analysis.findLonger(text)+ "\n");
-        result.append("Все годы, избегая повторений: " + Analysis.findYears(text) + "\n");
-        result.append("Главные герои: " + hero1.toString() + hero2.toString() + "\n\n");
+        result.append("Most rare word: " + Analysis.findLeastRecent(text)+ "\n");
+        result.append("Most common word: " + Analysis.findMostRecent(text)+ "\n");
+        result.append("Longest word: " + Analysis.findLonger(text)+ "\n");
+        result.append("All the years, avoiding repetition: " + Analysis.findYears(text) + "\n");
+        result.append("Main characters: " + hero1.toString() + hero2.toString() + "\n\n");
         return result;
     }
 }
