e6a91ec1da1e3e3972dea41467147922dbbfa979
ElizavetaKisel <lizavetakisel00@gmail.com>
2021-11-23 20:48:37 +0300

    write and read result

--- src/main/java/by/overone/homework27/Main.java
+++ src/main/java/by/overone/homework27/Main.java
@@ -1,9 +1,6 @@
 package by.overone.homework27;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
 
@@ -18,11 +15,19 @@ public class Main {
         }
         inputStream.close();
         String text = stringBuilder.toString();
-        System.out.println("*Romeo & Juliet*");
-        System.out.println("Самое редко встречающееся слово: " + Analysis.findLeastRecent(text));
-        System.out.println("Самое часто встречающееся слово: " + Analysis.findMostRecent(text));
-        System.out.println("Самое длинное слово: " + Analysis.findLonger(text));
-        System.out.println("Все годы, избегая повторений: " + Analysis.findYears(text) + "\n");
+
+        FileOutputStream outputStream = new FileOutputStream("result.txt");
+        StringBuilder result = new StringBuilder();
+
+        result.append("*Romeo & Juliet*"+ "\n");
+        result.append("Самое редко встречающееся слово: " + Analysis.findLeastRecent(text)+ "\n");
+        result.append("Самое часто встречающееся слово: " + Analysis.findMostRecent(text)+ "\n");
+        result.append("Самое длинное слово: " + Analysis.findLonger(text)+ "\n");
+        result.append("Все годы, избегая повторений: " + Analysis.findYears(text) + "\n\n");
+
+        byte[] bytes = result.toString().getBytes();
+        outputStream.write(bytes);
+        outputStream.close();
 
         FileReader reader = new FileReader("voina_i_mir.txt");
         StringBuilder stringBuilder1 = new StringBuilder();
@@ -32,11 +37,26 @@ public class Main {
         }
         reader.close();
         String text2 = stringBuilder1.toString();
-        System.out.println("*Война и мир*");
-        System.out.println("Самое редко встречающееся слово: " + Analysis.findLeastRecent(text2));
-        System.out.println("Самое часто встречающееся слово: " + Analysis.findMostRecent(text2));
-        System.out.println("Самое длинное слово: " + Analysis.findLonger(text2));
-        System.out.println("Все годы, избегая повторений: " + Analysis.findYears(text2));
 
+        FileWriter writer = new FileWriter("result.txt", true);
+        StringBuilder result2 = new StringBuilder();
+
+        result2.append("*Война и мир*"+ "\n");
+        result2.append("Самое редко встречающееся слово: " + Analysis.findLeastRecent(text2)+ "\n");
+        result2.append("Самое часто встречающееся слово: " + Analysis.findMostRecent(text2)+ "\n");
+        result2.append("Самое длинное слово: " + Analysis.findLonger(text2)+ "\n");
+        result2.append("Все годы, избегая повторений: " + Analysis.findYears(text2)+ "\n\n");
+
+        writer.write(result2.toString());
+        writer.close();
+
+        FileReader readRes = new FileReader("result.txt");
+        StringBuilder stringResult = new StringBuilder();
+        int r;
+        while ((r=readRes.read()) != -1){
+            stringResult.append((char) r);
+        }
+        reader.close();
+        System.out.println(stringResult);
     }
 }
