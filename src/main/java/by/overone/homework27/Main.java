87e8a3ab741ff5547c6ce5a4493fad8a3fbe7cc3
ElizavetaKisel <lizavetakisel00@gmail.com>
2021-11-23 12:38:19 +0300

    add txt

--- src/main/java/by/overone/homework27/Main.java
+++ src/main/java/by/overone/homework27/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        FileInputStream inputStream = new FileInputStream("D://5 Курсы overone//Romeo&Juliet.txt");
+        FileInputStream inputStream = new FileInputStream("Romeo&Juliet.txt");
         StringBuilder stringBuilder = new StringBuilder();
         int i;
         while ((i = inputStream.read()) != -1){
@@ -16,6 +16,8 @@ public class Main {
         }
         inputStream.close();
         String text = stringBuilder.toString();
-
+        System.out.println(Analysis.findLeastRecent(text));
+        System.out.println(Analysis.findMostRecent(text));
+        System.out.println(Analysis.findLonger(text));
     }
 }
